5866dc26e173f64751ce96ed12cb3f0a09885836
giuppiRC <giuseppe.funicello@redcarbon.ai>
2022-03-23 15:54:39 +0100

    wip

--- pages/_document.js
+++ pages/_document.js
@@ -4,16 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        ></link>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Fredoka&display=swap"
-          rel="stylesheet"
-        />
+      
       </Head>
       <body>
         <Main />
